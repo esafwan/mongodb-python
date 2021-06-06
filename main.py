8cf30a32da5414523d37326a300c4f9b898fb411
Safwan E <safwan@tridz.com>
2021-06-07 02:05:50 +0530

    Added example for use with authentication

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import urllib
 
 # import pymongo library to access mongodb 
 import pymongo
@@ -14,10 +15,32 @@ host=os.getenv('host')
 port=os.getenv('port')
 database=os.getenv('database')
 
+
+
+
+##########################################
+# FOR MONGODB WITH NO AUTHENTICATION.
+# COMMENT THE BELOW BLOCK & UNCOMMENT NEXT 
+# BLOCK IF YOU USE USERNAME & PASSWORD
+##########################################
+
 # Set connection url to connect
 # Example: "mongodb://13.233.190.38:27017/"
-connection_url = "mongodb://" + host + ":" + "27017/"
+connection_url = 'mongodb://%s:%s/' % (host, port)
 mongo = pymongo.MongoClient(connection_url)
+
+
+##########################################
+# FOR MONGODB WITH AUTHENTICATION.
+# UNCOMMENT THE BELOW CODE BLOCK & COMMENT 
+# PREVIOUS CODE BLOCK.
+##########################################
+
+# username=urllib.parse.quote_plus(os.getenv('username')) 
+# password=urllib.parse.quote_plus(os.getenv('password'))
+
+# connection_url = 'mongodb://%s:%s@%s:%s/' % (username, password, host, port)
+# mongo = pymongo.MongoClient(connection_url)
 
 # Get list of all databases from mongodb
 dblist = mongo.list_database_names()
@@ -33,7 +56,7 @@ else:
 
 # Connect to existing DB or create DB.
 # Note: Database will not be created until first set of data is written.
-db = mongo["safwan"]
+db = mongo[database]
 
 # Get a collection that exist or create a new collections
 # Collections are like tables in MySQL and stores data as documents(somewhat like rows)
@@ -43,10 +66,10 @@ users_collection = db["user"]
 # Insert a sample user data to user collections
 
 # Sample user data (dict)
-user = { "name": "John", "address": "Highway 37" }
+user_data = { "name": "John", "address": "Highway 37" }
 
 # Insert the data to users_collection
-res = users_collection.insert_one(user)
+res = users_collection.insert_one(user_data)
 
 # Print Response 
 print(res.inserted_id)
